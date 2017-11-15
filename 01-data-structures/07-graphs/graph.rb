b9978afb561b7ff1f6e9a2a5a904afca0f3c079c
rrsg123 <rrsg123@gmail.com>
2017-11-15 15:31:21 -0700

    correction to 7

--- 01-data-structures/07-graphs/graph.rb
+++ 01-data-structures/07-graphs/graph.rb
@@ -2,26 +2,17 @@ include 'node'
 
 class Graph
 	def find_kevin_bacon(actor)	
-	  
-	  bacon_films  = []
-	  bacon_actors = []
-
-	  #all the films KB has been in
-	  Kevin_Bacon.film_actor_hash.each do |film,actors|
-	  	bacon_films.push(film)
-	  end
-
-	  #all the actors in all the films KB has been in
-	  Kevin_Bacon.film_actor_hash.each do |film,actors|
-	  	bacon_actors.push(actors)
-	  end
-
-	  #here I will cycle through all the actors the target has been involved with and narrow it down to KB, if possible
+	
+	  #here I will cycle through all the actors the target has been involved with 
 	  connections = []
 	  remainder_1 = []
 	  remainder_2 = []
 	  remainder_3 = []
 	  
+	  #three sets of remainders because the produced array will always start on a movie 
+	  #and end on a movie (if we're linking an actor to another actor, those are the only edges)
+
+
 
 	  actor.film_actor_hash.each do |film, actors|
 	    actors.each do |actor|
@@ -47,7 +38,7 @@ class Graph
 	  	  actor.film_actor_hash.each do |film, actors|
 	  	  	film.each do |actor|
 	  	  		if actor == Kevin_Bacon
-	  	  			connections.push(remainder_1[actor])
+	  	  			connections.push(remainder_2[actor])
 	  	  			connections.push(film)
 	  	  		else
 	  	  			remainder_3.push(actor)
@@ -59,7 +50,7 @@ class Graph
 	  	  actor.film_actor_hash.each do |film, actors|
 	  	  	film.each do |actor|
 	  	  		if actor == Kevin_Bacon
-	  	  			connections.push(remainder_1[actor])
+	  	  			connections.push(remainder_3[actor])
 	  	  			connections.push(film)
 	  	  		else
 	  	  			print "There is no connection!"
