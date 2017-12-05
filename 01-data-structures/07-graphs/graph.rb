6214f725ca45b997e0de1162679e261f9cf55142
rrsg123 <rrsg123@gmail.com>
2017-12-05 14:54:43 -0700

    update7

--- 01-data-structures/07-graphs/graph.rb
+++ 01-data-structures/07-graphs/graph.rb
@@ -1,69 +1,58 @@
-require_relative 'node'
-
 class Graph
-	def find_kevin_bacon(name)	
-	
-	  #here I will cycle through all the actors the target has been involved with 
+
+	def self.find_kevin_bacon(actor)	
+	  @film_actor_hash = Hash.new()
+
 	  connections = []
-	  remainder_1 = []
-	  remainder_2 = []
-	  remainder_3 = []
-	  
-	  #three sets of remainders because the produced array will always start on a movie 
-	  #and end on a movie (if we're linking an actor to another actor, those are the only edges)
+	  kb_films    = []
+	  actor_films = []
+	 
+	  #All of Kevin Bacon's films
+	  @film_actor_hash.each do |key,value|	  	
+	  	if value == "Kevin Bacon"
+	  	  kb_films.push(key)
+	  	end
+	  end
 
-
-
-	  film_actor_hash.each do |film, actors|
-	    actors.each do |actor|
-	      if actor == Kevin_Bacon
-	      	connections.push(film)
-	      else
-	      	remainder_1.push(actor)
-	      end
+	  #All the films target actor has been in
+	  @film_actor_hash.each do |key,value|
+	    if value == actor
+	      actor_films.push(key)
 	    end
-	  end  
-	
-	  	remainder_1.each do |actor|
-	  	  actor.each do |film, actors|
-	  	  	film.each do |actor|
-	  	  		if actor == Kevin_Bacon
-	  	  			connections.push(remainder_1[actor])
-	  	  			connections.push(film)
-	  	  		else
-	  	  			remainder_2.push(actor)
-	  	  		end
-	  	  	end
-	  	  end
-	  	end
-	
-	  	remainder_2.each do |actor|
-	  	  actor.each do |film, actors|
-	  	  	film.each do |actor|
-	  	  		if actor == Kevin_Bacon
-	  	  			connections.push(remainder_2[actor])
-	  	  			connections.push(film)
-	  	  		else
-	  	  			remainder_3.push(actor)
-	  	  		end
-	  	  	end
-	  	  end
-	  	end
-	
-	  	remainder_3.each do |actor|
-	  	  actor.each do |film, actors|
-	  	  	film.each do |actor|
-	  	  		if actor == Kevin_Bacon
-	  	  			connections.push(remainder_3[actor])
-	  	  			connections.push(film)
-	  	  		else
-	  	  			print "There is no connection or connection is longer than 6 edges!"
-	  	  		end
-	  	  	end
-	  	  end
-	  	end
-	
+	  end
 
-	print connections	
+	  film_in_common     = kb_films & actor_films
+	  kb_actors          = @film_actor_hash.values_at(kb_films)
+	  actor_actors       = @film_actor_hash.values_at(actor_films)
+	  
+	  four_edge_2           = actor_actors & kb_actors
+	  four_edge_1           = (@film_actor_hash.key(four_edge_2) & actor_films)
+	  four_edge_3           = (@film_actor_hash.key(four_edge_2) & kb_films)
+
+	  six_edge_3           = @film_actor_hash.key(actor_actors) & @film_actor_hash.key(kb_actors)
+	  six_edge_2           = six_edge_3 & actor_actors
+	  six_edge_4           = six_edge_3 & kb_actors
+	  six_edge_1           = actor_films & six_edge_2
+	  six_edge_5           = kb_films & six_edge_4
+
+	  #The following should produce 4 different results:
+	  #1. If the target and Kevin Bacon appear in the same movie, only that movie will be pushed (2 edge result)
+	  #2. If not, then all movies with KB and all movies with target are cross checked for common actors (4 edges)
+	  #3. If still no matches, all movies 2 edges from both movie sets (which are already 1 edge away from the ends) will be checked (total of 6 edges)
+	  #4. If still nothing, output should reflect the lack of connection.
+
+ 	  # If both actors appear in the same film
+ 	  if film_in_common.length == 1
+ 	    #in this case, there will only be one movie in common, so one item pushed	
+ 	    connections.push(film_in_common) 
+ 	  # If there is a common actor that connections a film both the target and KB has been in 
+ 	  elsif film_in_common == nil && four_edge_2.length == 1
+ 	  	connections.push(four_edge_1, four_edge_2, four_edge_3)
+ 	  elsif four_edge_2 == nil && six_edge_3 != nil 
+ 	  	connections.push(six_edge_1, six_edge_2, six_edge_3, six_edge_4, six_edge_5)
+ 	  else
+ 	   	connections.push("There is no connection, or the connections is longer than 6 edges")
+ 	  end
+	puts connections	
 	end
 end
--- 01-data-structures/07-graphs/graph_spec.rb
+++ 01-data-structures/07-graphs/graph_spec.rb
@@ -1,6 +1,5 @@
 include RSpec
 
-require_relative 'node'
 require_relative 'graph'
 
 RSpec.describe Graph, type: Class do
@@ -8,51 +7,61 @@ RSpec.describe Graph, type: Class do
 
   describe "find_kevin_bacon" do
 	it "returns the number of edges connecting passed actor to Kevin_Bacon" do
-      film_actor_hash                              = Hash.new
-      film_actor_hash["Footloose"]                 = ["Kevin_Bacon", "Lori_Singer", "Dianne_Wiest", "John_Lithgow", "Sarah_Jessica_Parker", "Chris_Penn"]
-      film_actor_hash["The Man with One Red Shoe"] = ["Tom_Hanks", "Dabney_Coleman", "Lori_Singer", "Charles_Durning", "Carrie_Fisher", "Jim_Belushi"]
-      film_actor_hash["Hannah and Her Sisters"]    = ["Barbara_Hershey", "Dianne_Wiest", "Woody_Allen", "Julie_Kavner", "Ira_Wheeler", "Joanna_Gleeson"]
-      film_actor_hash["Annie Hall"]                = ["Woody_Allen", "Diane_Keaton", "Tony_Roberts", "Carol_Kane", "Paul_Simon", "Janet_Margolin"]   
-      film_actor_hash["The Godfather"]             = ["Marlon_Brando", "Al_Pacino", "James_Caan", "Robert_Duvall", "Diane_Keaton", "John_Cazale"]
-      film_actor_hash["Random Movie"]              = ["Random_Actor1", "Random_Actor2", "Random_Actor3", "Random_Actor4", "Random_Actor5", "Random_Actor6"]
-   
-      expect(Graph.find_kevin_bacon("Dabney_Coleman")).to eq ["The Man with One Red Shoe", "Lori_Singer", "Footloose"]
+	  film_actor_hash                              = {	
+      
+      'Footloose'                  => ['Kevin Bacon', 'Lori Singer', 'Dianne Wiest', 'John Lithgow', 'Sarah Jessica Parker', 'Chris Penn'],
+      'The Man with One Red Shoe'  => ['Tom Hanks', 'Dabney Coleman', 'Lori Singer', 'Charles Durning', 'Carrie Fisher', 'Jim Belushi'],
+      'Hannah and Her Sisters'     => ['Barbara Hershey', 'Dianne Wiest', 'Woody Allen', 'Julie Kavner', 'Ira Wheeler', 'Joanna Gleeson'],
+      'Annie Hall'                 => ['Woody Allen', 'Diane Keaton', 'Tony Roberts', 'Carol Kane', 'Paul Simon', 'Janet Margolin'],   
+      'The Godfather'              => ['Marlon Brando', 'Al Pacino', 'James Caan', 'Robert Duvall', 'Diane Keaton', 'John Cazale'],
+      'Random Movie'               => ['Random Actor1', 'Random Actor2', 'Random Actor3', 'Random Actor4', 'Random Actor5', 'Random Actor6']
+      
+      }
+      
+      expect(Graph.find_kevin_bacon('Dabney_Coleman')).to eq ['The Man with One Red Shoe', 'Lori_Singer', 'Footloose']
     end
 
     it "returns the number of edges connecting passed actor to Kevin_Bacon" do
-      film_actor_hash                              = Hash.new
-      film_actor_hash["Footloose"]                 = ["Kevin_Bacon", "Lori_Singer", "Dianne_Wiest", "John_Lithgow", "Sarah_Jessica_Parker", "Chris_Penn"]
-      film_actor_hash["The Man with One Red Shoe"] = ["Tom_Hanks", "Dabney_Coleman", "Lori_Singer", "Charles_Durning", "Carrie_Fisher", "Jim_Belushi"]
-      film_actor_hash["Hannah and Her Sisters"]    = ["Barbara_Hershey", "Dianne_Wiest", "Woody_Allen", "Julie_Kavner", "Ira_Wheeler", "Joanna_Gleeson"]
-      film_actor_hash["Annie Hall"]                = ["Woody_Allen", "Diane_Keaton", "Tony_Roberts", "Carol_Kane", "Paul_Simon", "Janet_Margolin"]   
-      film_actor_hash["The Godfather"]             = ["Marlon_Brando", "Al_Pacino", "James_Caan", "Robert_Duvall", "Diane_Keaton", "John_Cazale"]
-      film_actor_hash["Random Movie"]              = ["Random_Actor1", "Random_Actor2", "Random_Actor3", "Random_Actor4", "Random_Actor5", "Random_Actor6"]
-    
-      expect(Graph.find_kevin_bacon("Tony_Roberts")).to eq ["Annie Hall", Woody_Allen, "Hannah and Her Sisters", Dianne_Wiest, "Footloose"]
+      film_actor_hash                              = {	
+      
+      'Footloose'                  => ['Kevin Bacon', 'Lori Singer', 'Dianne Wiest', 'John Lithgow', 'Sarah Jessica Parker', 'Chris Penn'],
+      'The Man with One Red Shoe'  => ['Tom Hanks', 'Dabney Coleman', 'Lori Singer', 'Charles Durning', 'Carrie Fisher', 'Jim Belushi'],
+      'Hannah and Her Sisters'     => ['Barbara Hershey', 'Dianne Wiest', 'Woody Allen', 'Julie Kavner', 'Ira Wheeler', 'Joanna Gleeson'],
+      'Annie Hall'                 => ['Woody Allen', 'Diane Keaton', 'Tony Roberts', 'Carol Kane', 'Paul Simon', 'Janet Margolin'],   
+      'The Godfather'              => ['Marlon Brando', 'Al Pacino', 'James Caan', 'Robert Duvall', 'Diane Keaton', 'John Cazale'],
+      'Random Movie'               => ['Random Actor1', 'Random Actor2', 'Random Actor3', 'Random Actor4', 'Random Actor5', 'Random Actor6']
+      
+      }
+      expect(Graph.find_kevin_bacon('Tony_Roberts')).to eq ['Annie Hall', 'Woody_Allen', 'Hannah and Her Sisters', 'Dianne_Wiest', 'Footloose']
     end
 
     it "returns the number of edges connecting passed actor to Kevin_Bacon" do
-      film_actor_hash                              = Hash.new
-      film_actor_hash["Footloose"]                 = ["Kevin_Bacon", "Lori_Singer", "Dianne_Wiest", "John_Lithgow", "Sarah_Jessica_Parker", "Chris_Penn"]
-      film_actor_hash["The Man with One Red Shoe"] = ["Tom_Hanks", "Dabney_Coleman", "Lori_Singer", "Charles_Durning", "Carrie_Fisher", "Jim_Belushi"]
-      film_actor_hash["Hannah and Her Sisters"]    = ["Barbara_Hershey", "Dianne_Wiest", "Woody_Allen", "Julie_Kavner", "Ira_Wheeler", "Joanna_Gleeson"]
-      film_actor_hash["Annie Hall"]                = ["Woody_Allen", "Diane_Keaton", "Tony_Roberts", "Carol_Kane", "Paul_Simon", "Janet_Margolin"]   
-      film_actor_hash["The Godfather"]             = ["Marlon_Brando", "Al_Pacino", "James_Caan", "Robert_Duvall", "Diane_Keaton", "John_Cazale"]
-      film_actor_hash["Random Movie"]              = ["Random_Actor1", "Random_Actor2", "Random_Actor3", "Random_Actor4", "Random_Actor5", "Random_Actor6"]
-    
-      expect(Graph.find_kevin_bacon("Marlon_Brando")).to eq "There is no connection or connection is longer than 6 edges!"
+      film_actor_hash                              = {	
+      
+      'Footloose'                  => ['Kevin Bacon', 'Lori Singer', 'Dianne Wiest', 'John Lithgow', 'Sarah Jessica Parker', 'Chris Penn'],
+      'The Man with One Red Shoe'  => ['Tom Hanks', 'Dabney Coleman', 'Lori Singer', 'Charles Durning', 'Carrie Fisher', 'Jim Belushi'],
+      'Hannah and Her Sisters'     => ['Barbara Hershey', 'Dianne Wiest', 'Woody Allen', 'Julie Kavner', 'Ira Wheeler', 'Joanna Gleeson'],
+      'Annie Hall'                 => ['Woody Allen', 'Diane Keaton', 'Tony Roberts', 'Carol Kane', 'Paul Simon', 'Janet Margolin'],   
+      'The Godfather'              => ['Marlon Brando', 'Al Pacino', 'James Caan', 'Robert Duvall', 'Diane Keaton', 'John Cazale'],
+      'Random Movie'               => ['Random Actor1', 'Random Actor2', 'Random Actor3', 'Random Actor4', 'Random Actor5', 'Random Actor6']
+      
+      }
+      expect(Graph.find_kevin_bacon('Marlon_Brando')).to eq "There is no connection or connection is longer than 6 edges!"
     end
 
     it "returns the number of edges connecting passed actor to Kevin_Bacon" do
-      film_actor_hash                              = Hash.new
-      film_actor_hash["Footloose"]                 = ["Kevin_Bacon", "Lori_Singer", "Dianne_Wiest", "John_Lithgow", "Sarah_Jessica_Parker", "Chris_Penn"]
-      film_actor_hash["The Man with One Red Shoe"] = ["Tom_Hanks", "Dabney_Coleman", "Lori_Singer", "Charles_Durning", "Carrie_Fisher", "Jim_Belushi"]
-      film_actor_hash["Hannah and Her Sisters"]    = ["Barbara_Hershey", "Dianne_Wiest", "Woody_Allen", "Julie_Kavner", "Ira_Wheeler", "Joanna_Gleeson"]
-      film_actor_hash["Annie Hall"]                = ["Woody_Allen", "Diane_Keaton", "Tony_Roberts", "Carol_Kane", "Paul_Simon", "Janet_Margolin"]   
-      film_actor_hash["The Godfather"]             = ["Marlon_Brando", "Al_Pacino", "James_Caan", "Robert_Duvall", "Diane_Keaton", "John_Cazale"]
-      film_actor_hash["Random Movie"]              = ["Random_Actor1", "Random_Actor2", "Random_Actor3", "Random_Actor4", "Random_Actor5", "Random_Actor6"]
-    
-      expect(Graph.find_kevin_bacon("Random_Actor4")).to eq "There is no connection or connection is longer than 6 edges!"
+      film_actor_hash                              = {	
+      
+      'Footloose'                  => ['Kevin Bacon', 'Lori Singer', 'Dianne Wiest', 'John Lithgow', 'Sarah Jessica Parker', 'Chris Penn'],
+      'The Man with One Red Shoe'  => ['Tom Hanks', 'Dabney Coleman', 'Lori Singer', 'Charles Durning', 'Carrie Fisher', 'Jim Belushi'],
+      'Hannah and Her Sisters'     => ['Barbara Hershey', 'Dianne Wiest', 'Woody Allen', 'Julie Kavner', 'Ira Wheeler', 'Joanna Gleeson'],
+      'Annie Hall'                 => ['Woody Allen', 'Diane Keaton', 'Tony Roberts', 'Carol Kane', 'Paul Simon', 'Janet Margolin'],   
+      'The Godfather'              => ['Marlon Brando', 'Al Pacino', 'James Caan', 'Robert Duvall', 'Diane Keaton', 'John Cazale'],
+      'Random Movie'               => ['Random Actor1', 'Random Actor2', 'Random Actor3', 'Random Actor4', 'Random Actor5', 'Random Actor6']
+      
+      }
+      expect(Graph.find_kevin_bacon('Random_Actor4')).to eq "There is no connection or connection is longer than 6 edges!"
     end
+  
   end
 end  
